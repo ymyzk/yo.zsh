97ddd00542932c07e7247c15db81eb102174dfdf
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-10-25 01:22:05 +0900

    Support text option

--- yo.zsh
+++ yo.zsh
@@ -5,72 +5,80 @@ YO_API_URL='https://api.justyo.co/yo/'
 YO_HISTORY_FILE="$HOME/.yo_zsh_history"
 
 function sendyo() {
-  local location url username
+  local location text url username
   local -a usernames
   local request_command message response
   local -a success_usernames
-  local opt
+  local -A opts
   local color_clear='\e[0m'
   local color_ok='\e[0;32m'
   local color_fail='\e[0;31m'
   local tmpfile=$(mktemp 2>/dev/null || mktemp -t yo_zsh)
 
-  # Parse arguments
-  for opt in "$@"; do
-    case $opt in
-      '-h'|'--help')
-        echo "usage: $0 [options] username ..."
-        echo
-        echo "Options:"
-        echo "  --help, -h"
-        echo "      Show help"
-        echo "  --location {lat,lon}, -l {lat,lon}"
-        echo "      Send Yo Location"
-        echo "  --url {url}, -u {url}"
-        echo "      Send Yo Link"
-        return 1
-        ;;
-      '-l'|'--location')
-        if [[ -z "$2" ]]; then
-          echo "requires argument"
-          return 1
-        fi
-        location="$2"
-        shift 2
-        ;;
-      '-u'|'--url')
-        if [[ -z "$2" ]]; then
-          echo "requires argument"
-          return 1
-        fi
-        url="$2"
-        shift 2
-        ;;
-      -*)
-        echo "illegal option"
-        return 1
-        ;;
-      *)
-        if [[ -n "$1" ]]; then
-          usernames+=($(echo $1 | tr '[:lower:]' '[:upper:]'))
-          shift 1
-        fi
-        ;;
-    esac
+  # Parse options
+  zparseopts -D -A opts h -help l: -location: u: -url: t: -text:
+
+  if [[ -n "${opts[(i)-h]}" ]] || [[ -n "${opts[(i)--help]}" ]]; then
+    echo "usage: $0 [options] username ..."
+    echo
+    echo "Options:"
+    echo "  --help, -h"
+    echo "      Show help"
+    echo "  --location {lat,lon}, -l {lat,lon}"
+    echo "      Send Yo Location"
+    echo "  --url {url}, -u {url}"
+    echo "      Send Yo Link"
+    echo "  --text {text}, -t {text}"
+    echo "      Send Yo with Text"
+    return 1
+  fi
+
+  if [[ -n "${opts[(i)-l]}" ]]; then
+    location="$opts[-l]"
+  fi
+  if [[ -n "${opts[(i)--location]}" ]]; then
+    location="$opts[--location]"
+  fi
+
+  if [[ -n "${opts[(i)-u]}" ]]; then
+    url="$opts[-u]"
+  fi
+  if [[ -n "${opts[(i)--url]}" ]]; then
+    url="$opts[--url]"
+  fi
+
+  if [[ -n "${opts[(i)-t]}" ]]; then
+    text="$opts[-t]"
+  fi
+  if [[ -n "${opts[(i)--text]}" ]]; then
+    text="$opts[--text]"
+  fi
+
+  for username in "$@"; do
+    usernames+=($(echo $1 | tr '[:lower:]' '[:upper:]'))
   done
 
+  # Send
   for username in $usernames; do
     # Prepare request
     request_command="curl --silent --write-out %{http_code} --output ${tmpfile} -d api_token=$YO_API_TOKEN -d username=$username $YO_API_URL"
+
     if [[ -n "$location" ]]; then
-      message="Yo Location! $location $username"
+      message="Yo Location! $location"
       request_command="$request_command -d 'location=$location'"
     elif [[ -n "$url" ]]; then
-      message="Yo Link! $url $username"
+      message="Yo Link! $url"
       request_command="$request_command -d 'link=$url'"
     else
-      message="Yo! $username"
+      message="Yo!"
     fi
+
+    if [[ -n "$text" ]]; then
+      message="$message with \"$text\""
+      request_command="$request_command -d 'text=$text'"
+    fi
+
+    message="$message $username"
 
     # Send request
     printf "[....] $message"
@@ -123,6 +131,7 @@ function _sendyo() {
     '(-h --help)'{-h,--help}'[Show help]' \
     '(-l --location -u --url)'{-l,--location}'[Send Yo Location {lat,lon}]:coordinate:()' \
     '(-l --location -u --url)'{-u,--url}'[Send Yo Link]:url:_urls' \
+    '(-t --text)'{-t,--text}'[Send Yo with Text]:text:()' \
     '*:args:_sendyo_args'
 }
 
